ee02afe6158b36932b23d9c58a98c6a7793f3808
maheshsingh20 <114943084+maheshsingh20@users.noreply.github.com>
2022-11-24 12:00:23 +0530

    Add files via upload
    Project for creating hospital website

---
+++ edx.py
@@ -0,0 +1,65 @@
+
+# x="193"
+# print(type(x)) for checking type of x
+# int(x)
+# x=int(X)
+# print(type(x))
+# float(x)
+# x=float(x)
+# print(type(x))
+# x=complex(x)
+# print(type(x))
+# a=89
+# print(complex(a,b)) to create complex numbers
+# print(abs(a)) for cheking absolute value mean modulus value
+# import math
+# a=10
+# print(math.exp(a)) to create exponential function
+# import math
+# a=(12,46,894,9876,687)
+# print(math.sqrt(a)) to check under root of any number
+# print(max(a)) for checking maximum value
+# print(min(a)) 
+
+#  list and creating list
+
+# num=[1,2,3,4]
+# print(num) number list
+# letter=['a','c','y']
+# print(letter) letter list
+# var=["mahi","harry","taniya"]
+# print(var) creating list of strings
+# combi=[1,6,'y','t',"mahi","taniya"]
+# print(combi)
+# matrices
+# mat=[[1,2],['a','b'],[9,'j']]
+# print(mat)
+
+
+# accessing elements in list
+
+
+# a=[1,9,'l',"mahi","hobart","india"]
+# print(a[3])
+# print(a[-3])
+# print(a[-2])
+# print(a[5])
+# print(a[:4])
+# print(a[-2:])
+# print(a[:-3])
+# print(len(a)) to checking the length of the list
+# print(a[::2]) when we want second term by skipping one
+# print(a[::3]) for 3rd one
+# print(a[::-1]) to reverse the direction of the list
+
+# operation on list
+
+
+# x=[0]*100
+# print(x) print 0 100 times
+# letter=['a','b','c']
+# num=[1,2,3,]
+# print(letter+num)
+
+# var=list("hey there")
+# print(var) for making list of each letters of the string
---
+++ list.py
@@ -0,0 +1,50 @@
+# letter=['a','b','c']
+# stg=["mahi","kal","hub"]
+# conc=letter+stg
+# print(conc) for adding two list in a single list
+
+
+# num=int(input(("num: ")))
+# table=[num*i for i in range(1,15)]
+# print(table) for creating table in range in which you want
+
+
+# num=[7,2,3,4]
+# one,*other=num for taking out that number out of the list
+# print(one)
+# print(other)
+
+# methods in the list
+# num=[1,2,3,4,7] 
+# num.append(6) for adding 6 into list , only one charcter could be add
+# print(num)
+
+
+# num=[1,2,3,4,5]
+# stg=["mahi","tanu"]
+# num.extend(stg)
+# print(num) for adding one list intp others
+
+# num=[1,2,3,7,9]
+# num.insert(3,"mahi")
+# print(num)  for inserting the charcter in between of the list
+
+# remove m,ethod
+
+# num=[9,2,3,4,0]
+# num.remove(3)
+# print(num) for removing  the charcter from the list
+
+# var1=['w','d','e','y','a']
+# var1.sort()
+# print(var1)  for  arranging list in proper order
+
+# Bulit in functions 
+
+x=[12,343,54,667,8779,45,94]
+# len(x)
+# print(len(x))
+# print(min(x)) minimum value
+# print(max(x)) maximum value
+# print(sum(x)) sum of the list
+# print(sum(x)/len(x)) average value of the list
